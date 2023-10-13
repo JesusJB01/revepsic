ddf0b0749f2352183728560b1ada887b24a67e90
JesusJB01 <abelbracho40@gmail.com>
2023-10-13 17:50:36 -0500

    eliminado sudb

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Description from "@/components/Description";
 import Hero from "@/components/Hero";
-import Sliders from "@/components/Sliders";
 import Newsletter from "@/components/Newsletter";
 import ArticlesHome from "@/components/ArticlesHome";
 
@@ -11,9 +10,7 @@ export default function page() {
     <div className="pt-20  flex flex-col items-center justify-center">
     <Hero/>
     <Description/>
-    
     <Newsletter/>
-    {/* <Sliders/> */}
    <ArticlesHome/>  
   </div>
   );
