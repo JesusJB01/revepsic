f339496493d011a50bc9c2128ae96f2318822432
JesusJB01 <abelbracho40@gmail.com>
2023-10-14 10:43:31 -0500

    arreglando links de article

--- components/Article.tsx
+++ components/Article.tsx
@@ -26,7 +26,7 @@ export default function Article({ title, imageSrc, summary, slug}: { title: stri
         <h2 className="pt-4 pb-2 text-lg font-bold">{title}</h2>
         <p className="text-justify pb-4">{summary}</p>
         <div>
-        <Link href={ `${api}/blog/${slug}`} className="bg-violet-600 px-4 py-2 rounded-lg text-white">
+        <Link href={ "/blog/${slug}"} className="bg-violet-600 px-4 py-2 rounded-lg text-white">
     Leer más
   </Link>
         </div>
